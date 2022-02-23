859ee51d718ecb0a57e4528862400d158bcd9516
Christopher Gioia <christopher.g@brillio.com>
2022-02-23 13:24:43 -0800

    add some suite files

---
+++ src/test/java/ecommerce/automation/base/ExampleTest.java
@@ -0,0 +1,13 @@
+package ecommerce.automation.base;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class ExampleTest extends BaseTest {
+
+    @Test
+    public void test() {
+        assertEquals(2, 2);
+    }
+}
